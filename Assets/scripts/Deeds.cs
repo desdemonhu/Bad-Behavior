bc9055b6d07dd80e339367da18b173486ceafa55
Raegan Millhollin <desdemonhu@gmail.com>
2018-03-08 15:45:22 -0800

    Added faction stuff and hooked it into dialogue treegit add .!

---
+++ Assets/scripts/Deeds.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PixelCrushers.LoveHate;
+
+public class Deeds : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public virtual void Disappointed(GameObject target)
+    {
+        GetComponent<DeedReporter>().ReportDeed("disappointed", target.GetComponent<FactionMember>());
+
+        var temperament = target.GetComponent<FactionMember>().pad.GetTemperament();        print(temperament);
+    }
+
+    public virtual void Flatter(GameObject target)
+    {
+        GetComponent<DeedReporter>().ReportDeed("flatter", target.GetComponent<FactionMember>());
+    }
+}
